ef3e2526a87ba2e4b2cef90e89b54a642e46801b
hgs1217 <mf20080540@126.com>
2017-11-22 11:26:16 +0800

    init

---
+++ config.py
@@ -0,0 +1,9 @@
+# @Author:      HgS_1217_
+# @Create Date: 2017/11/20
+
+
+UPLOAD_IMAGE_FOLDER = "E:/DB_upload/image"
+UPLOAD_VIDEO_FOLDER = "E:/DB_upload/video"
+
+ALLOWED_IMAGE_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_VIDEO_EXTENSIONS = set(['avi', 'mov', 'mp4', 'wmv', 'mpg'])
---
+++ main.py
@@ -0,0 +1,55 @@
+# @Author:      HgS_1217_
+# @Create Date: 2017/11/20
+
+from flask import Flask, request, jsonify
+from config import UPLOAD_IMAGE_FOLDER, UPLOAD_VIDEO_FOLDER, ALLOWED_IMAGE_EXTENSIONS, ALLOWED_VIDEO_EXTENSIONS
+import time
+
+
+app = Flask("ClothesImageProcessing")
+app.secret_key = "sjtu"
+
+
+def allowed_img(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_IMAGE_EXTENSIONS
+
+
+def allowed_video(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_VIDEO_EXTENSIONS
+
+
+@app.route('/')
+def main():
+    return "Welcome"
+
+
+@app.route("/upload/img", methods=['POST'])
+def upload_img():
+    file = request.files['image']
+    if file and allowed_img(file.filename):
+        current_time = time.localtime()
+        filename = time.strftime("%Y%m%d%H%M%S", current_time) + '.' + file.filename.rsplit('.', 1)[1]
+        path = UPLOAD_IMAGE_FOLDER + "/" + filename
+        file.save(path)
+        dic = {"fileName": filename, "path": path, "error": 0, "msg": "Upload file success"}
+        return jsonify(dic), 200
+    dic = {"error": 1, "msg": "file error or unsupported file format"}
+    return jsonify(dic), 404
+
+
+@app.route("/upload/video", methods=['POST'])
+def upload_video():
+    file = request.files['file']
+    if file and allowed_video(file.filename):
+        current_time = time.localtime()
+        filename = time.strftime("%Y%m%d%H%M%S", current_time) + '.' + file.filename.rsplit('.', 1)[1]
+        path = UPLOAD_VIDEO_FOLDER + "/" + filename
+        file.save(path)
+        dic = {"fileName": filename, "path": path, "error": 0, "msg": "Upload file success"}
+        return jsonify(dic), 200
+    dic = {"error": 1, "msg": "file error or unsupported file format"}
+    return jsonify(dic), 404
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
