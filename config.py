72af703c8379641f6b4ffeb5b8d0aeae9ebb8010
hgs1217 <mf20080540@126.com>
2017-11-29 10:27:23 +0800

    add socket comm

--- config.py
+++ config.py
@@ -2,6 +2,11 @@
 # @Create Date: 2017/11/20
 
 
+SOCKET_HOST = "202.120.38.141"
+SOCKET_PORT = 8080
+
+SOCKET_REPLY_PATH = 'E:/a.txt'
+
 UPLOAD_IMAGE_FOLDER = "E:/DB_upload/image"
 UPLOAD_VIDEO_FOLDER = "E:/DB_upload/video"
 
--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 # @Create Date: 2017/11/20
 
 from flask import Flask, request, jsonify, send_from_directory, abort
-from config import UPLOAD_IMAGE_FOLDER, UPLOAD_VIDEO_FOLDER, ALLOWED_IMAGE_EXTENSIONS, ALLOWED_VIDEO_EXTENSIONS
+from config import UPLOAD_IMAGE_FOLDER, UPLOAD_VIDEO_FOLDER, ALLOWED_IMAGE_EXTENSIONS, \
+    ALLOWED_VIDEO_EXTENSIONS, SOCKET_HOST, SOCKET_PORT, SOCKET_REPLY_PATH
 import os
 import time
+import socket
+import codecs
 
 
 app = Flask("ClothesImageProcessing")
@@ -17,6 +20,16 @@ def allowed_img(filename):
 
 def allowed_video(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_VIDEO_EXTENSIONS
+
+
+def socket_communication(cmd):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.sendto(cmd, (SOCKET_HOST, SOCKET_PORT))
+
+
+def get_socket_reply():
+    with codecs.open("E:/a.txt", "r", encoding="gbk") as f:
+        return f.read()
 
 
 @app.route('/')
@@ -59,6 +72,56 @@ def get_img(img_path):
         folder, img_name = "/".join(dirs[:-1]), dirs[-1]
         return send_from_directory(folder, img_name, as_attachment=True)
     abort(404)
+
+
+@app.route("/socket/posture", methods=['GET'])
+def posture_analysis():
+    gender = request.args.get("gender")
+    tp = request.args.get("type")
+    img_url = request.args.get("imgUrl")
+    cmd = ";".join(["2", gender, tp, img_url]) + ";"
+
+    try:
+        socket_communication(cmd)
+        reply = get_socket_reply()
+
+        [x, y, w, h] = reply.split(";")[0:4]
+        dic = {"x": x, "y": y, "width": w, "height": h, "error": 0, "msg": "Posture analysis success"}
+        return jsonify(dic), 200
+    except:
+        dic = {"error": 2, "msg": "Posture analysis error"}
+        return jsonify(dic), 404
+
+
+@app.route("/socket/once_search", methods=['GET'])
+def once_search():
+    gender = request.args.get("gender")
+    tp = request.args.get("type")
+    img_url = request.args.get("imgUrl")
+    x = request.args.get("x")
+    y = request.args.get("y")
+    width = request.args.get("width")
+    height = request.args.get("height")
+    alo = request.args.get("alo")
+    cmd = ";".join(["0", gender, tp, x, y, width, height, img_url, alo]) + ";"
+
+    try:
+        socket_communication(cmd)
+        reply = get_socket_reply()
+
+        reply_list = reply[:-1].split(";")
+        h, s, v, attr_num = reply_list[0], reply_list[1], reply_list[2], reply_list[3]
+        attrs = reply_list[4:4+attr_num]
+        imgs = reply_list[4+attr_num:]
+        img_list = list(zip(*[iter(imgs)]*3))
+        img_dics = [{"imgUrl": img[0], "imgWidth": img[1], "imgHeight": img[2]} for img in img_list]
+
+        dic = {"h": h, "s": s, "v": v, "attrs": attrs, "images": img_dics,
+               "error": 0, "msg": "Once search success"}
+        return jsonify(dic), 200
+    except:
+        dic = {"error": 3, "msg": "Once search error"}
+        return jsonify(dic), 404
 
 
 if __name__ == '__main__':
